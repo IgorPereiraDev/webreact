d5713ef3ea2894cbcf0a8953281fef9f2e9a389c
IgorPereiraDev <pereirachapa2001@gmail.com>
2020-02-13 13:56:36 -0300

    Aplicando bootstrap com tabela

--- src/components/series/FormularioSeries.js
+++ src/components/series/FormularioSeries.js
@@ -27,24 +27,29 @@ class FormularioSeries extends Component{
 
     render(){
         return(
-            <div>
-                <form method="post" onSubmit={this.enviaDados}>
-                    <div className="form">
-                            <label htmlFor="nome">Nome</label>
-                            <input type="text" id="nome" name="nome" value={this.state.nome} onChange={this.inputHandler}></input>
+            <div className="card">
+                <div className="card-header">
+                    Cadastro de series
+                </div>
+                <div className="card-body">
+                    <form method="post" onSubmit={this.enviaDados}>
+                        <div className="form-group">
+                                <label htmlFor="nome">Nome</label>
+                                <input className="form-control" type="text" id="nome" name="nome" value={this.state.nome} onChange={this.inputHandler}></input>
 
-                            <label htmlFor="lancamento">Ano de lancamento</label>
-                            <input type="text" id="lancamento" name="ano_lancamento" value={this.state.ano_lancamento} onChange={this.inputHandler}></input>
+                                <label htmlFor="lancamento">Ano de lancamento</label>
+                                <input className="form-control" type="text" id="lancamento" name="ano_lancamento" value={this.state.ano_lancamento} onChange={this.inputHandler}></input>
 
-                            <label htmlFor="temporadas">Temporadas</label>
-                            <input type="text" id="temporadas" name="temporadas" value={this.state.temporadas} onChange={this.inputHandler}></input>
+                                <label htmlFor="temporadas">Temporadas</label>
+                                <input className="form-control" type="text" id="temporadas" name="temporadas" value={this.state.temporadas} onChange={this.inputHandler}></input>
 
-                            <label htmlFor="sinopse">Sinopse</label>
-                            <textarea id="sinopse" name="sinopse" value={this.state.sinopse} onChange={this.inputHandler}></textarea>
+                                <label htmlFor="sinopse">Sinopse</label>
+                                <textarea className="form-control" id="sinopse" name="sinopse" value={this.state.sinopse} onChange={this.inputHandler}></textarea>
 
-                            <button type="submit">Salvar</button>
-                    </div>
-                </form>
+                                <button className="btn btn-success form-control mt-2" type="submit">Salvar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
