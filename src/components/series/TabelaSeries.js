c2e6c368a3ae2b080d774949d1aa8a92d7d5ac29
IgorPereiraDev <pereirachapa2001@gmail.com>
2020-02-13 16:52:55 -0300

    Usando bootstrap

--- src/components/series/TabelaSeries.js
+++ src/components/series/TabelaSeries.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
+import './TabelaSeries.css'
 
-const TabelaHead = () => {
-    return (
-        <thead className="thead-dark">
-            <tr>
-                <th>Nome</th>
-                <th>Lançamento</th>
-                <th>Temporadas</th>
-                <th>Sinopse</th>
-            </tr>
-        </thead>
-    )
-}
+const ListaSeries = (props) => {
 
-const TabelaBody = (props) => {
     return(
-        <tbody>
+        <div className="card-body card-body-flex">
             {props.series.map(serie => {
                 return (
-                    <tr key={serie.id}>
-                        <td>{serie.nome}</td>
-                        <td>{serie.ano_lancamento}</td>
-                        <td>{serie.temporadas}</td>
-                        <td>{serie.sinopse}</td>
-                    </tr>
+                    <div className="card card-serie bg-dark text-white" key={serie.id}>
+                        <div className="card-header" >
+                            <h5 className="card-title text-center">{serie.nome}</h5>
+                            <h6 className="card-title text-muted mb-0">{serie.ano_lancamento}</h6>
+                        </div>
+                        <div className="card-body">
+                            <img src="/logo192.png" className="card-img"/>
+                        </div>
+                        <div className="card-footer">
+                            {serie.temporadas}
+                            {serie.temporadas > 1 ? ' temporadas' : ' temporada'}
+                            <br/>
+                            <a href="#">Sinopse</a>
+                            <br/>
+                            <div className="text-center mt-1" onClick={() => {
+                                if(window.confirm('Confirma a exclusão'))
+                                props.deleta(serie.id)
+                                }}>
+                                <button className="btn btn-outline-danger btn-sm mr-2 p-1">Delete</button>
+                                <button className="btn btn-outline-warning btn-sm p-1">Editar</button>
+                            </div>
+                        </div>
+                    </div>
                 )
             })}
-        </tbody>
+        </div>
     )
 }
 
@@ -34,19 +40,14 @@ class TabelaSeries extends Component{
 
     render (){
 
-        const { series } = this.props
+        const { series, deleta } = this.props
 
         return (
             <div className="card">
                 <div className="card-header">
-                    Lista de series
+                    <h5 className="text-center">Lista de series</h5>
                 </div>
-                <div className="card-body">
-                    <table className="table table-striped table-hover">
-                        <TabelaHead/>
-                        <TabelaBody series={series}/>	
-                    </table>
-                </div>
+                    <ListaSeries series={series} deleta={deleta}/>
 			</div>
         )
     }
